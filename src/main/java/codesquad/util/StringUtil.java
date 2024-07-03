f7eb5dcf12b9e6f1e1d08cd41d848a2467a44cf6
CodingLuizy <luizy991212@gmail.com>
2024-07-03 09:46:34 +0900

    feature: 상수를 정의한 String util 생성

---
+++ src/main/java/codesquad/util/StringUtil.java
@@ -0,0 +1,12 @@
+package codesquad.util;
+
+public final class StringUtil {
+
+    public static final String SP = " ";
+    public static final char CR = '\r';
+    public static final char LF = '\n';
+    public static final String CRLF = "\r\n";
+
+    private StringUtil() {
+    }
+}
