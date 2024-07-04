7e3ca1d9f3a4afab73c3273a7ad29e712035abe1
luizy <luizy991212@gmail.com>
2024-07-05 02:15:13 +0900

    feature: User Database 등록
    - 유저 정보를 저장하기 위한 ConcurrentMap 사용
    - record 객체 json으로 반환하기 위한 변환 객체 추가

---
+++ src/main/java/codesquad/dao/user/User.java
@@ -0,0 +1,69 @@
+package codesquad.dao.user;
+
+import java.util.Objects;
+
+
+public class User {
+    private Long id;
+    private String userId;
+    private String nickname;
+    private String password;
+
+    public User() {
+    }
+
+    public User(String userId, String nickname, String password) {
+        this.userId = userId;
+        this.nickname = nickname;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public ImmutableUser toImmutableUser() {
+        return new ImmutableUser(id, userId, nickname, password);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
