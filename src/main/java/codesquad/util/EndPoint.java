5c069c1672a079c12023994d0bc0800f3bed9781
luizy <luizy991212@gmail.com>
2024-07-05 02:33:26 +0900

    feature: path에 패턴 매칭이 가능하도록 변경
    - 향후 parh variable을 인식할 수 있도록
    - 동작 검증을 하는 테스트 코드 추가

--- src/main/java/codesquad/util/EndPoint.java
+++ src/main/java/codesquad/util/EndPoint.java
@@ -2,9 +2,44 @@ package codesquad.util;
 
 import codesquad.http11.HttpMethod;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public record EndPoint(HttpMethod method, String path) {
+    private static final String PATH_VARIABLE_REGEX = "\\{[^/]+}";
 
     public static EndPoint of(HttpMethod method, String path) {
         return new EndPoint(method, path);
+    }
+
+    public boolean matches(String method, String path) {
+        if (!this.method.name().equalsIgnoreCase(method)) {
+            return false;
+        }
+        Pattern pattern = Pattern.compile(convertPathToRegex(this.path));
+        Matcher matcher = pattern.matcher(path);
+        return matcher.matches();
+    }
+
+    public Matcher getMatcher(String path) {
+        Pattern pattern = Pattern.compile(convertPathToRegex(this.path));
+        return pattern.matcher(path);
+    }
+
+    private static String convertPathToRegex(String path) {
+        return path.replaceAll(PATH_VARIABLE_REGEX, "([^/]+)");
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        EndPoint endPoint = (EndPoint) o;
+        return method.equals(endPoint.method) && path.equals(endPoint.path);
+    }
+
+    @Override
+    public int hashCode() {
+        return path.hashCode();
     }
 }
