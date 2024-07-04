f6ea660b589902d768a585f9b9540a025ae55a76
luizy <luizy991212@gmail.com>
2024-07-05 02:16:02 +0900

    feature: User Router 추가
    - 등록 api 추가
    - 유저 확인 api 추가

---
+++ src/main/java/codesquad/router/UserCreateRouter.java
@@ -0,0 +1,35 @@
+package codesquad.router;
+
+import codesquad.dao.user.User;
+import codesquad.dao.user.UserDao;
+import codesquad.http11.HttpStatus;
+
+import java.util.List;
+import java.util.Map;
+
+public class UserCreateRouter extends Router{
+    private static final UserCreateRouter instance = new UserCreateRouter();
+    private UserCreateRouter() {}
+    public static UserCreateRouter getInstance() {return instance;}
+
+    private final UserDao userDao = UserDao.getInstance();
+
+
+    {
+        path = "/create";
+
+        get = (request, response) -> {
+            Map<String, List<String>> queryParams = request.queryParams();
+            if (!queryParams.containsKey("userId") || !queryParams.containsKey("password") || !queryParams.containsKey("name")) {
+                response.setStatus(HttpStatus.BAD_REQUEST);
+                return "required parameters are missing";
+            }
+            String userId = queryParams.get("userId").get(0);
+            String password = queryParams.get("password").get(0);
+            String name = queryParams.get("name").get(0);
+            User user = new User(userId, name, password);
+            user = userDao.save(user);
+            return user.toImmutableUser();
+        };
+    }
+}
