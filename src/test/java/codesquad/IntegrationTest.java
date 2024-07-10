69b00f20dac8444a89aa9a46ffb84dedb9e31244
CodingLuizy <luizy991212@gmail.com>
2024-07-10 10:50:50 +0900

    test: 통합테스트를 위한 뼈대 코드 완성
    - `@BeforeAll` 로 가상의 서버 띄우기
    - `Configuration` 를 기반으로 서버 생성

---
+++ src/test/java/codesquad/IntegrationTest.java
@@ -0,0 +1,37 @@
+package codesquad;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import server.Server;
+import server.config.Configuration;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class IntegrationTest {
+
+    private static final Configuration configuration = new ServerConfiguration();
+    private static final Client client = new Client(configuration);
+
+    @BeforeAll
+    public static void setUp() throws InterruptedException {
+        new Thread(() -> {
+            try {
+                Server server = new Server(configuration);
+                server.start();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+        Thread.sleep(1000);
+    }
+
+    @Test void testClientResponse() throws IOException {
+        Response response = client.sendRequest("GET", "/luizy", null, null);
+
+        assertEquals(200, response.responseCode());
+        assertEquals("OK", response.responseMessage());
+        assertEquals("Hello, Luizy!", response.responseBody());
+    }
+}
